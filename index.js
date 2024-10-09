547a1e04bb1e424d6a7c3ad88d5487760a1f1a38
Damian Molter <damian.molter.programista@gmail.com>
2024-10-09 14:06:11 +0200

    Now buttons react on click

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var colorNames = ["green", "red", "yellow", "blue"];
+var buttonColours = ["green", "red", "yellow", "blue"];
 var gamePattern = [];
 var colorNumber = nextSequence();
+var chosenColorNumber;
 
 function nextSequence(){
       var randomNumber = Math.random();
@@ -8,7 +9,28 @@ function nextSequence(){
       return colorNumber;
 }
 
-$(document).click(function(){
-      colorNumber = nextSequence();
-      gamePattern.push(colorNumber);
+
+$(".btn").click(function(){
+      //console.log(this);
+      //colorNumber = nextSequence();
+      //gamePattern.push(colorNumber);
+      var chosenColorNumber;
+      switch(this.id){
+            case "green":
+                  chosenColorNumber = 0;
+                  break;
+                  case "red":
+                  chosenColorNumber = 1;
+                  break;
+                  case "yellow":
+                  chosenColorNumber = 2;
+                  break;
+                  case "blue":
+                  chosenColorNumber = 3;
+                  break;
+      }
+
+      console.log(chosenColorNumber);
+
 })
+
