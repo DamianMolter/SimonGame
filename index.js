221950a0dc03bf3a048b4fac7ef335646990a523
Damian Molter <damian.molter.programista@gmail.com>
2024-10-09 13:31:19 +0200

    Added array next elements generating by left click.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-var colorNumber = nextSequence();
 var colorNames = ["green", "red", "yellow", "blue"];
+var gamePattern = [];
+var colorNumber = nextSequence();
 
 function nextSequence(){
       var randomNumber = Math.random();
       var colorNumber = Math.floor(randomNumber * 4);
       return colorNumber;
 }
+
+$(document).click(function(){
+      colorNumber = nextSequence();
+      gamePattern.push(colorNumber);
+})
